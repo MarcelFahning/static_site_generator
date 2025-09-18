ff1a30daa9c2e62fa5c9d63ec5b0b75f22399e71
Marcel Fahning <marcel.fahning@planet-ai.de>
2025-09-18 15:54:44 +0200

    -

---
+++ src/textnode.py
@@ -0,0 +1,17 @@
+class TextNode:
+    def __init__(self, text: str, text_type, url: str):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, text_node) -> bool:
+        if (
+            self.text == text_node.text
+            and self.text_type == text_node.text_type
+            and self.url == text_node.url
+        ):
+            return True
+        return False
+    
+    def __repr__(self):
+        return f'TextNode({self.text}, {self.text_type}, {self.url})'
