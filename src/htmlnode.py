60188fcb6df708bc57fb66f02584415cafaa5fc7
Marcel Fahning <marcel.fahning@planet-ai.de>
2025-09-29 13:29:47 +0200

    -

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag: str=None, value: str=None, children: list=None, props: dict=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(selfself):
+        raise NotImplementedError
+
+    def props_to_html(self) -> str:
+        result = ""
+        for prop in self.props.items():
+            result += f'{prop}={self.props[prop]}'
+        return f'href={self.props["href"]} target={self.props["target"]}'
+
+    def __repr__(self):
+        return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
+
+
---
+++ src/test_htmlnode.py
@@ -0,0 +1,21 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href": "https://www.google.com","target": "_blank",})
+        self.assertEqual(node.props_to_html(), 'href="https://www.google.com" target="_blank"')
+
+    def test_repr(self):
+        node = HTMLNode('b', 'bold text')
+        self.assertEqual(print(node), print('HTMLNode("b", "bold text)'))
+
+    def test_repr2(self):
+        node = HTMLNode('b', 'bold text')
+        self.assertEqual(print(node), print('HTMLNode("b", "bold text)'))
+
+
+if __name__ == "__main__":
+    unittest.main()
