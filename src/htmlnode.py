8784f92d26317c2365eac6b76335f011ccd66a09
Marcel Fahning <marcel.fahning@planet-ai.de>
2025-09-30 14:53:00 +0200

    -

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,18 @@ class HTMLNode:
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
 
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError('Leaf node must have a value')
+        if self.tag is None:
+            return f'{self.value}'
+        else:
+            s = f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+            return s
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
